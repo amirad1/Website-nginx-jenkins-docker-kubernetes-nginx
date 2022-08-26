c8896c2a67841d800ea7969ee7d1171459a71c71
amirad1 <105715253+amirad1@users.noreply.github.com>
2022-08-26 20:40:31 +0430

    Add files via upload

---
+++ agent.groovy
@@ -0,0 +1,51 @@
+pipeline {
+agent none
+    stages {
+        stage('Example Build') {
+            agent {
+                label 'docker'
+            }
+            steps {
+                sh 'docker-compose up --build -d'
+            }
+        }
+        stage('Test image existence') {
+            agent {
+                label 'docker'
+            }
+            steps{
+                sh 'cd /root/docker && bash image-test.sh'
+            }
+        }
+        stage('Test running container') {
+            agent {
+                label 'docker'
+            }
+            steps{
+                sh 'cd /root/docker && bash container-test.sh'
+            }
+        }
+        stage('Push to Docker hub'){
+            agent { label 'docker' }
+            steps{
+                sh 'docker push site'
+            }
+        }
+            stage('deploy'){
+        agent {
+                label 'kubernetes'
+    }
+                steps{
+                    sh 'cd /root && kubectl apply -f dep.yaml'
+            }
+        }
+    }
+    post {
+        always {
+            junit 'reports.xml'
+        }
+        success {
+            echo "pipeline executed sucsessfuly"
+        }
+    }
+}
